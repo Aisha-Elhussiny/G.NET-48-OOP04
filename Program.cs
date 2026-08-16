77b304d442e223508e3e6ab1b37698986b679969
Aisha-Elhussiny <aishaahmedelhussiny@gmail.com>
2026-08-16 23:18:31 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            #region Q1)Abstraction
+            #region Q1) Abstraction
             #region a) What is Abstraction in Object-Oriented Programming?
-            // its focuses on simplifying complex systems by modeling classes based on the essential properties and behaviors an object should have, while hiding unnecessary implementation details. It allows developers to create abstract classes and interfaces that define a blueprint for derived classes, enabling them to implement specific functionality without exposing the underlying complexity.
+            // it's focuses on simplifying complex systems by modeling classes based on the essential properties and behaviors an object should have, while hiding unnecessary implementation details. It allows developers to create abstract classes and interfaces that define a blueprint for derived classes, enabling them to implement specific functionality without exposing the underlying complexity.
             #endregion
             #region b)  Why is abstraction considered one of the four pillars of OOP?
             // because it reduces complexity, protects implementation details, and allows the programmer to focus on what an object does instead of how it does it.
